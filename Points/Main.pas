cf9d12eafad29408b002ed5a75673fad16a30715
Artemmr <rib.artem@gmail.com>
2016-10-29 00:24:17 +0500

    Stupid metod added

--- Points/Main.pas
+++ Points/Main.pas
@@ -3,7 +3,7 @@
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,System.Diagnostics,
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,System.Diagnostics,Math,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit, FMX.EditBox, FMX.NumberBox,
   FMX.Menus;
@@ -32,11 +32,10 @@ type
     procedure PointsMaxChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Lab1GenerateClick(Sender: TObject);
-    procedure Lab1PAClick(Sender: TObject);
-    procedure Lab1PBClick(Sender: TObject);
     procedure PoleMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
     procedure PoleMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+    procedure Lab1StupidClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -44,13 +43,14 @@ type
   end;
   TSelector = record
     RectAB:TRectF;
-    SelectA,SelectB:Boolean;
+    Top,Right,Bottom,Left:Single;
+    P1,P2,P3,P4:TPointF;
   end;
 var
   MainWindow: TMainWindow;
   Points: Array of TPointF;
   MaxX,MaxY: integer;
-  PointsNumber:integer;
+  PointsCount:integer;
   Stopwatch:TStopwatch;
   Selector:TSelector;
 implementation
@@ -58,10 +58,20 @@ implementation
 {$R *.fmx}
 {$R *.LgXhdpiPh.fmx ANDROID}
 {$R *.Windows.fmx MSWINDOWS}
-
+//------------------------------------------------------------------------------
+//NaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaN_WATMAN
+//------------------------------------------------------------------------------
 procedure TMainWindow.PointsMaxChange(Sender: TObject);
 begin
-  PointsNumber:=Trunc(PointsMax.Value);
+  PointsCount:=Trunc(PointsMax.Value);
+end;
+
+procedure SetBorder();
+begin
+  Selector.Left:=Math.MinValue([Selector.RectAB.Left,Selector.RectAB.Right]);
+  Selector.Right:=Math.MaxValue([Selector.RectAB.Left,Selector.RectAB.Right]);
+  Selector.Top:=Math.MinValue([Selector.RectAB.Top,Selector.RectAB.Bottom]);
+  Selector.Bottom:=Math.MaxValue([Selector.RectAB.Top,Selector.RectAB.Bottom]);
 end;
 
 //Иницилизация при открытии
@@ -69,19 +79,20 @@ procedure TMainWindow.FormCreate(Sender: TObject);
 var
   PoleRect:TRectF;
 begin
-  PointsNumber:=Trunc(PointsMax.Value);
+  PointsCount:=Trunc(PointsMax.Value);
   MaxY:=Trunc(Pole.Height);
   MaxX:=Trunc(Pole.Width);
+
   Pole.Bitmap.Width:=Trunc(Pole.Width);
   Pole.Bitmap.Height:=Trunc(Pole.Height);
   PoleRect.Create(0,0,MaxX,MaxY);
 
-  Canvas.Clear(TAlphaColors.White);
-  Canvas.Stroke.Color := TColorRec.Black;
-  Canvas.Stroke.Kind:= TBrushKind.None;
-  Canvas.Stroke.Thickness := 1;
   with Pole.Bitmap.Canvas do
   begin
+    //Clear(TAlphaColors.White);
+    //Stroke.Color := TColorRec.Black;
+    //Stroke.Kind:= TBrushKind.None;
+    //Stroke.Thickness := 1;
     BeginScene;
     DrawRect(PoleRect,0,0,AllCorners,100);
     //DrawLine(my_point_1, my_point_2, 1.0);
@@ -98,9 +109,9 @@ var
   i:Integer;
   x,y:Extended;
 begin
-  SetLength(Points,PointsNumber);
+  SetLength(Points,PointsCount);
   //Stopwatch.Start;
-  for i := 1 to PointsNumber do
+  for i := 1 to PointsCount do
   begin
     X:=Random(MaxX-100)+50-0.5;
     Y:=Random(MaxY-100)+50-0.5;
@@ -118,26 +129,36 @@ i:Integer;
 begin
   //SetLength(TempPoints,PointsNumber);
   //TempPoints[i]:=PointF(Points[i].X-(Stroke.Thickness/2),Points[i].Y-(Stroke.Thickness/2));
-  Stopwatch.Reset;
-  Stopwatch.Start;
   with MainWindow.Pole.Bitmap.Canvas do
   begin
     BeginScene;
     Clear(TAlphaColors.White);
     //DrawLine(Points[1],Points[5],1);
-    for i := 1 to PointsNumber do
+    for i := 1 to PointsCount do
       //DrawLine(TempPoints[i],TempPoints[i],100);
       DrawLine(Points[i],Points[i],100);
     DrawRect(Selector.RectAB,0,0,AllCorners,100);
     EndScene;
   end;
-  Stopwatch.Stop;
-  MainWindow.Edit1.Text:=IntToStr(Stopwatch.ElapsedMilliseconds);
 end;
 
 procedure StupidCompute();
+var
+  i,Count: Integer;
 begin
   //--
+  SetBorder();
+  Count:=0;
+  Stopwatch.Reset;
+  Stopwatch.Start;
+  for i := 1 to PointsCount do
+  //begin
+    if math.InRange(Points[i].X,Selector.Left,Selector.Right) and math.InRange(Points[i].Y,Selector.Top,Selector.Bottom)
+    then Inc(Count);
+  //end;
+  Stopwatch.Stop;
+  MainWindow.Edit2.Text:=IntToStr(Stopwatch.ElapsedMilliseconds);
+  MainWindow.Edit1.Text:=FloatToStr(Count);
 end;
 
 procedure SmartComputePre();
@@ -152,8 +173,9 @@ end;
 
 procedure CheckRect();
 begin
+  SetBorder();
   with MainWindow do
-  Edit2.Text:=FloatToStr(Selector.RectAB.Top);
+  //Edit2.Text:=FloatToStr(Selector.Left);//FloatToStr(Selector.RectAB.Top);
   //StatusBar.
   //Selector.RectAB.Top
 end;
@@ -173,15 +195,9 @@ begin
   GeneratePoints();
 end;
 
-procedure TMainWindow.Lab1PAClick(Sender: TObject);
+procedure TMainWindow.Lab1StupidClick(Sender: TObject);
 begin
-  //SelectPoints.Top
-  Selector.SelectA:=true;
-end;
-
-procedure TMainWindow.Lab1PBClick(Sender: TObject);
-begin
-  Selector.SelectB:=true;
+  StupidCompute();
 end;
 
 procedure TMainWindow.PoleMouseDown(Sender: TObject; Button: TMouseButton;
