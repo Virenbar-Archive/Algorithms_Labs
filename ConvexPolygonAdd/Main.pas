e245cbef8475d8e75a5187e1f0de8a5f67d9fc00
Artemmr <rib.artem@gmail.com>
2016-12-29 15:46:13 +0500

    Попытка создания алгоритма реального времени

--- ConvexPolygonAdd/Main.pas
+++ ConvexPolygonAdd/Main.pas
@@ -43,7 +43,10 @@ type
   public
     { Public declarations }
   end;
-
+  PointAngle = record
+    ID:   Integer;
+    Angle:Extended;
+  end;
   TPointFArray = Array of TPointF;
 var
   MainWindow:   TMainWindow;
@@ -51,12 +54,15 @@ var
   MaxX,MaxY:    Integer;
   PointsCount:  Integer;
   Stopwatch:    TStopwatch;
+  AngleComparer:IComparer<PointAngle>;
+  HullRTList:   TList<PointAngle>;
+  HullRTCenter: TPointF;
 
 implementation
 
 {$R *.fmx}
 {$R *.LgXhdpiPh.fmx ANDROID}
-{$R *.Windows.fmx MSWINDOWS}
+
 //------------------------------------------------------------------------------
 //NaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaNNaN_WATMAN
 //------------------------------------------------------------------------------
@@ -71,6 +77,23 @@ begin
   Result:=PointF(Floor(Point.X)+0.5,Floor(Point.Y)+0.5);
 end;
 
+//Скалярное произведение
+function Scalar(P1ID,P2ID,P3ID:Integer):Integer;
+var
+  S:Extended;
+  P1,P2,P3:TPointF;
+begin
+  P1:=Points[P1ID];
+  P2:=Points[P2ID];
+  P3:=Points[P3ID];
+  //Scal:=((P2.X-P1.X)*(P3.X-P1.X))+((P2.Y-P1.Y)*(P3.Y-P1.Y));//Не работает
+  //Scal:=((P2.X-P1.X)*(P3.Y-P1.Y))-((P2.Y-P1.Y)*(P3.X-P1.X));
+  S:=((P2.X-P1.X)*(-P3.Y+P1.Y))-((-P2.Y+P1.Y)*(P3.X-P1.X));//Y Инвертирован
+  if S<0 then Result:=-1;
+  if S=0 then Result:=0;
+  if S>0 then Result:=1;
+end;
+
 //Иницилизация при открытии
 procedure TMainWindow.FormCreate(Sender: TObject);
 var
@@ -83,6 +106,15 @@ begin
   Pole.Bitmap.Width:=Trunc(Pole.Width);
   Pole.Bitmap.Height:=Trunc(Pole.Height);
   PoleRect.Create(0,0,MaxX,MaxY);
+
+  {Сравниватель по углам}
+  AngleComparer:=TDelegatedComparer<PointAngle>.Create(
+  function(const Left, Right: PointAngle): Integer
+  begin
+    Result := Trunc(Left.Angle - Right.Angle);
+  end
+  );
+  HullRTList:=TList<PointAngle>.Create();
 
   with Pole.Bitmap.Canvas do
   begin
@@ -159,6 +191,55 @@ begin
     Result[i]:=PointF(X,Y);
   end;
   //DrawPoints();
+end;
+
+procedure GenerateHullRTList(Hull:TList<Integer>);
+var
+  i:          Integer;
+  Temp:       PointAngle;
+  x,y,x1,y1:  Extended;
+  A,SA,Cx,Cy: Extended;
+begin
+  HullRTList.Count:=Hull.Count;
+  //Centroid
+  SA:=0;
+  Cx:=0;
+  Cy:=0;
+  for i := 0 to Hull.Count-1-1 do
+  begin
+    x:=Points[Hull[i]].X;
+    y:=Points[Hull[i]].Y;
+    x1:=Points[Hull[i+1]].X;
+    y1:=Points[Hull[i+1]].Y;
+    A:= x*y1 - x1*y;
+    SA:=SA+A;
+    Cx:=Cx+(x + x1)*A;
+    Cy:=Cy+(y + y1)*A;
+  end;
+  x:=Points[Hull.Last].X;
+  y:=Points[Hull.Last].Y;
+  x1:=Points[Hull[0]].X;
+  y1:=Points[Hull[0]].Y;
+  A:= x*y1 - x1*y;
+  SA:=SA+A;
+  Cx:=Cx+(x + x1)*A;
+  Cy:=Cy+(y + y1)*A;
+
+  SA:=SA*0.5;
+  Cx:=Cx/(6.0*SA);
+  Cy:=Cy/(6.0*SA);
+  HullRTCenter:=PointF(Cx,Cy);
+  //ShowMessage(FloatToStr(Cx)+' '+FloatToStr(Cy));
+  //
+  for i:=0 to Hull.Count-1 do
+  begin
+    Temp.ID:=Hull[i];
+    X:=Points[Hull[i]].X-HullRTCenter.X;
+    Y:=Points[Hull[i]].Y-HullRTCenter.Y;
+    Temp.Angle:=RadToDeg(ArcTan2(Y,X));
+    HullRTList[i]:=Temp;
+  end;
+  //ShowMessage('');
 end;
 
 {------------------------------------------------------------------------------}
@@ -255,20 +336,20 @@ begin
   MainWindow.Pole.Bitmap.Canvas.DrawLine(Points[HullList.Items[HullList.Count-1]],Points[HullList.Items[0]],100);
   MainWindow.Pole.Bitmap.Canvas.EndScene();
   }
+  GenerateHullRTList(HullList);
   HullList.Destroy;
 end;
 
 {------------------------------------------------------------------------------}
 //Оптимальный метод
 procedure Graham();
-type
+{type
   PointAngle = record
     ID:Integer;
     Angle:Integer;
-  end;
+  end;}
 var
   Start,i       :Integer;
-  Comparer      :IComparer<PointAngle>;
   SortList      :TList<PointAngle>;
   HullList      :TList<Integer>;
   P0,P1,P2      :TPointF;
@@ -276,15 +357,7 @@ var
   Polygon       :TPolygon;
   T             :Integer;
 begin
-  {Сравниватель по углам}
-  Comparer:=TDelegatedComparer<PointAngle>.Create(
-  function(const Left, Right: PointAngle): Integer
-  begin
-    Result := Left.Angle - Right.Angle;
-  end
-  );
-  //---------
-  SortList:=TList<PointAngle>.Create(Comparer);
+  SortList:=TList<PointAngle>.Create(AngleComparer);
   HullList:=TList<Integer>.Create();
   // На старт!!
   Stopwatch.Reset;
@@ -311,7 +384,7 @@ begin
       P1:=PointF(0,-1);
       P2:=PointF(Points[i].X-Points[Start].X,Points[i].Y-Points[Start].Y);
       CurPoint.ID:=i;
-      CurPoint.Angle:=Round((P1.X*P2.X+P1.Y*P2.Y)/(sqrt(P1.X*P1.X+P1.Y*P1.Y)*sqrt(P2.X*P2.X+P2.Y*P2.Y))*1000000000);
+      CurPoint.Angle:=(P1.X*P2.X+P1.Y*P2.Y)/(sqrt(P1.X*P1.X+P1.Y*P1.Y)*sqrt(P2.X*P2.X+P2.Y*P2.Y))*1000000000;
       SortList.Add(CurPoint);
     end;
     SortList.Sort;{Сортировка}
@@ -355,18 +428,77 @@ begin
   begin
     BeginScene();
     DrawPolygon(Polygon,100);
-    Stroke.Color := TAlphaColors.Red;
-    for i := 0 to HullList.Count-1-1 do
-      DrawLine(NoNoNo(Points[HullList.Items[i]]),NoNoNo(Points[HullList.Items[i]]),100);
-    Stroke.Color := TAlphaColors.Black;
+    //Stroke.Color := TAlphaColors.Red;
+    //for i := 0 to HullList.Count-1-1 do
+      //DrawLine(NoNoNo(Points[HullList.Items[i]]),NoNoNo(Points[HullList.Items[i]]),100);
+    //Stroke.Color := TAlphaColors.Black;
     EndScene();
   end;
+
+  GenerateHullRTList(HullList);
   HullList.Destroy;
   SortList.Destroy;
 end;
+
 {------------------------------------------------------------------------------}
-procedure AddPoint();
+//Реальное время
+procedure AddPoint(NewPoints:TPointFArray);
+var
+  i,S,SN,SP:Integer;
+  Index:Integer;
+  Temp:PointAngle;
+  //P1,P2,P3:TPointF;
+  L,H,M:Integer;
+  Left,Right:Integer;
 begin
+  for i := 0 to Length(NewPoints)-1 do
+  begin
+    Inc(PointsCount);
+    SetLength(Points,Length(Points)+1);
+    Points[Length(Points)-1]:=NewPoints[i];
+    Temp.ID:=Length(Points)-1;
+    Temp.Angle:=RadToDeg(ArcTan2(NewPoints[i].Y-HullRTCenter.Y,NewPoints[i].X-HullRTCenter.X));
+    HullRTList.BinarySearch(Temp,Index,AngleComparer);
+    //ShowMessage(IntToStr(Index));
+    if (Index=0) or (Index=HullRTList.Count) then
+      S:=Scalar(HullRTList.Last.ID,HullRTList.First.ID,Temp.ID)
+    else
+      S:=Scalar(HullRTList[Index-1].ID,HullRTList[Index].ID,Temp.ID);
+    if S<=0 then Continue;//Точка внутри, справа от вектора по часовой
+    ShowMessage(IntToStr(S));
+
+    Right:=-1;
+    L:=0;H:=HullRTList.Count-1;
+    M:=(L+H) div 2;
+    while Right<0 do
+    begin
+      //M:=(L+H) div 2;
+      SN:=Scalar(Temp.ID,HullRTList[M].ID,HullRTList[M+1].ID);
+      SP:=Scalar(Temp.ID,HullRTList[M].ID,HullRTList[M-1].ID);
+      if (SN>0) and (SP>0) then Right:=M;
+      if (SN>0) and (SP<0) then L:=M;
+      if (SN<0) and (SP>0) then H:=M;
+      if (SN<0) and (SP<0) then M:=M+1 else M:=(L+H) div 2;
+      //break;
+    end;
+    ShowMessage(IntToStr(Right));
+
+    Left:=-1;
+    L:=0;H:=HullRTList.Count-1;
+    M:=(L+H) div 2;
+    while Left<0 do
+    begin
+      //M:=(L+H) div 2;
+      SN:=Scalar(Temp.ID,HullRTList[M].ID,HullRTList[M+1].ID);
+      SP:=Scalar(Temp.ID,HullRTList[M].ID,HullRTList[M-1].ID);
+      if (SN<0) and (SP<0) then Left:=M;
+      if (SN>0) and (SP<0) then H:=M;
+      if (SN<0) and (SP>0) then L:=M;
+      if (SN>0) and (SP>0) then M:=M+1 else M:=(L+H) div 2;
+      //break;
+    end;
+    ShowMessage(IntToStr(Left));
+  end;
 
 end;
 
@@ -377,12 +509,19 @@ procedure TMainWindow.Lab2GenerateClick(Sender: TObject);
 begin
   //StatusBar.tab
   PointsMax.ResetFocus;
-  if RadioS.IsChecked then GeneratePointsSquare();
-  if RadioC.IsChecked then GeneratePointsCircle();
-  if RadioG.IsChecked then begin
-    SetLength(Points,PointsCount);
-    Points:=GeneratePointsGauss(PointsCount);
-    DrawPoints();
+  if not AddMousePoint.IsChecked then
+  begin
+    if RadioS.IsChecked then GeneratePointsSquare();
+    if RadioC.IsChecked then GeneratePointsCircle();
+    if RadioG.IsChecked then begin
+      SetLength(Points,PointsCount);
+      Points:=GeneratePointsGauss(PointsCount);
+      DrawPoints();
+    end;
+  end
+  else
+  begin
+    //
   end;
 end;
 
@@ -404,7 +543,17 @@ end;
 procedure TMainWindow.Button1Click(Sender: TObject);
 var
   I:integer;
+  List1:TList<Integer>;
 begin
+  List1:=TList<Integer>.Create();
+  List1.Add(2);
+  List1.Add(4);
+  List1.Add(8);
+  List1.Add(16);
+  List1.Add(32);
+
+  List1.BinarySearch(15,I);
+  ShowMessage(IntToStr(I));
   {for I := 1 to 10 do
   begin
     GeneratePointsGauss();
@@ -426,15 +575,17 @@ end;
 
 procedure TMainWindow.PoleMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Single);
+var
+  Temp:TPointFArray;
 begin
   if AddMousePoint.IsChecked then
   begin
-    Inc(PointsCount);
-    SetLength(Points,Length(Points)+1);
-    Points[Length(Points)-1]:=PointF(X,Y);
-    //AddPoint();
-    DrawPoints();
-    Graham();
+    SetLength(Temp,1);
+    Temp[0]:=PointF(X,Y);
+    //DrawPoints();
+    AddPoint(Temp);
+    //DrawPoints();
+    //Graham();
   end;
 end;
 
